f619bdaa59248914f7ebd69021da202ff0c971c4
Lukas Theiler <lukas.theiler@gmail.com>
2016-01-12 20:49:51 +0100

    Initial Commit

---
+++ Hackathon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hackathon
+//
+//  Created by Lukas Theiler on 12.01.16.
+//  Copyright © 2016 Lukas Theiler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
